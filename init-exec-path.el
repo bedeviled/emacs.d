cd053872bd089f85b89a1fa7fcf174b55f282c29
J.T. Halbert <b3d3vil3d@gmail.com>
2012-04-27 16:31:51 -0400

    changed the behavior of set-exec-path-from-shell-PATH

--- init-exec-path.el
+++ init-exec-path.el
@@ -1,7 +1,6 @@
 ;; setup the paths
 (defun set-exec-path-from-shell-PATH ()
-  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo
- $PATH'")))
+  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
     (setenv "PATH" path-from-shell)
     (setq exec-path (split-string path-from-shell path-separator))))
 (if window-system (set-exec-path-from-shell-PATH))
